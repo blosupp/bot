ddab6965b47fffed22df6d44d4ce418c1cbb7ea2
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-10 00:04:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-# config.py
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # <= обязательно int!

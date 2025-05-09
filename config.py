3d25487a926ba93e34943a2c6ef9de92dc4f080e
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-09 23:43:54 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))

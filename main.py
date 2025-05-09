11e4004dd3f919600edf5abfb1e5609ded586d69
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-10 00:09:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from config import BOT_TOKEN
-from handlers import start, help  # импорты из твоих модулей
-
+from handlers import start, help, photo  # добавляй только один раз!
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
-from handlers import start
-dp.include_router(start.router)
 
-
-# Регистрируем хендлеры
+# ✅ Каждый router добавляется один раз
 dp.include_router(start.router)
 dp.include_router(help.router)
+dp.include_router(photo.router)
 
 async def main():
     print("✅ Бот запущен.")

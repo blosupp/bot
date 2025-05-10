b22b24b59401d04a84630e85caafc495f3588687
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-10 02:11:40 +0200

    Create access_control.py

---
+++ middleware/access_control.py
@@ -0,0 +1,15 @@
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+from typing import Callable, Awaitable, Dict, Any
+from access import add_user
+
+class AccessMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: Dict[str, Any]
+    ) -> Any:
+        user_id = event.from_user.id
+        add_user(user_id)  # регистрируем, если нет
+        return await handler(event, data)

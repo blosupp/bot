922859b5b4e0a40f1c3b3ff64d6e77351217173f
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-10 02:26:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# main.py
-
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
@@ -10,37 +8,23 @@ from db import init_db
 from access import init_access_db
 from middleware.access_control import AccessMiddleware
 
-# Импорт обработчиков
-from handlers import (
-    start,
-    help,
-    photo,
-    post,
-    settings,
-    admin_panel,
-    text
-)
+from handlers import start, help, photo, post, settings, admin_panel
 
-# Инициализация бота и диспетчера
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
-# Подключение middleware
-dp.message.middleware(AccessMiddleware())  # <-- добавили контроль доступа
+dp.message.middleware(AccessMiddleware())
 
-# Подключение роутеров
 dp.include_router(start.router)
 dp.include_router(help.router)
 dp.include_router(photo.router)
 dp.include_router(post.router)
 dp.include_router(settings.router)
 dp.include_router(admin_panel.router)
-dp.include_router(text.router)
 
-# Точка входа
 async def main():
-    init_db()         # база диалогов
-    init_access_db()  # база доступа
+    init_db()
+    init_access_db()
     print("✅ Бот запущен и готов к работе.")
     await dp.start_polling(bot)
 

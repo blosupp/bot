3d6712cfc34325bc58b471620f6a24a1dd411f6d
Khongor Indzheev <indjeev.kh@gmail.com>
2025-05-11 01:31:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 ADMIN_ID = int(os.getenv("ADMIN_ID"))  # <= обязательно int!
+
